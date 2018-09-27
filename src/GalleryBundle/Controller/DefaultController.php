52df0fd519b83026e81dbdb1d66f5312d9a15954
steve <stevekouna@gmail.com>
2018-09-27 12:45:22 +0100

    configuation of the gallery module (installation of assets).

--- src/GalleryBundle/Controller/DefaultController.php
+++ src/GalleryBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GalleryBundle:Default:index.html.twig');
+        return $this->render('@Gallery/Layouts/index.html.twig');
     }
 }
