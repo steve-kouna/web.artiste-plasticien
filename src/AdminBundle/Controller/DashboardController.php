acbc4212dad381d1de86b4de6e861c7b2525f5bb
steve <stevekouna@gmail.com>
2018-10-04 20:43:23 +0100

    modification de la sidebar

--- src/AdminBundle/Controller/DashboardController.php
+++ src/AdminBundle/Controller/DashboardController.php
@@ -5,10 +5,16 @@ namespace AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DashboardController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('@Admin/Layouts/dashboard/index.html.twig');
+class DashboardController extends Controller {
+
+    public function indexAction($token) {
+        $verify_token = $this->container->get('app.verify_token')->tokenTest($token, $this->container->getParameter('api_uri'));
+        if ($verify_token) {
+            return $this->render('@Admin/Layouts/dashboard/index.html.twig', [
+                        'token' => $token
+            ]);
+        }
+        return $this->redirectToRoute('account_homepage');
     }
+
 }
